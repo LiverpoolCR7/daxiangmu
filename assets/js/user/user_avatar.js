3d53e7910f5c43c7975cad4407db7c193cadca7d
Liverpool <3308628430@qq.com>
2021-08-18 09:58:12 +0800

    完成了所有操作

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -1,34 +1,35 @@
-// 1.1 获取裁剪区域的 DOM 元素
 $(function () {
-
+    var layer = layui.layer
 
     var $image = $('#image')
-    // 1.2 配置选项
     const options = {
-        // 纵横比
         aspectRatio: 1,
-        // 指定预览区域
         preview: '.img-preview'
     }
 
-    // 1.3 创建裁剪区域
     $image.cropper(options)
+
     $('#btnChooseImage').on('click', function () {
         $('#file').click()
     })
+
     $('#file').on('change', function (e) {
+        var filelist = e.target.files
+        if (filelist.length === 0) {
+            return layer.msg('请选择照片！')
+        }
+
         var file = e.target.files[0]
-        var newImgURL = URL.createObjectURL(file)
-        console.log("url:");
-        console.log(newImgURL);
+        var imgURL = URL.createObjectURL(file)
         $image
-            .cropper('destroy')      // 销毁旧的裁剪区域
-            .attr('src', newImgURL)  // 重新设置图片路径
-            .cropper(options)        // 重新初始化裁剪区域
+            .cropper('destroy')
+            .attr('src', imgURL)
+            .cropper(options)
     })
-    $('#btnUpload').on('click', function (e) {
+
+    $('#btnUpload').on('click', function () {
         var dataURL = $image
-            .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+            .cropper('getCroppedCanvas', {
                 width: 100,
                 height: 100
             })
@@ -40,7 +41,7 @@ $(function () {
                 avatar: dataURL
             },
             success: function (res) {
-                if (res.status != 0) {
+                if (res.status !== 0) {
                     return layer.msg('更换头像失败！')
                 }
                 layer.msg('更换头像成功！')
--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -20,15 +20,17 @@ $(function () {
                 if (res.status !== 0) {
                     return layer.msg('获取用户信息失败！')
                 }
-                console.log(res)
+
                 form.val('formUserInfo', res.data)
             }
         })
     }
+
     $('#btnReset').on('click', function (e) {
         e.preventDefault()
         initUserInfo()
     })
+
     $('.layui-form').on('submit', function (e) {
         e.preventDefault()
         $.ajax({
@@ -39,9 +41,9 @@ $(function () {
                 if (res.status !== 0) {
                     return layer.msg('更新用户信息失败！')
                 }
-                layer.msg('用户信息更新成功！')
+                layer.msg('更新用户信息成功！')
+                window.parent.getUserInfo()
             }
         })
-        window.parent.getUserInfo()
     })
 })
